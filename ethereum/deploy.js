9136e0c046bc995fe419175add86f2bcf467866b
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-18 16:43:31 -0700

    tweak deploy script

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -8,15 +8,17 @@ const provider = new HDWalletProvider(ETHEREUM_PRIV_KEY, SEPOLIA_TESTNET_ENDPOIN
 const web3 = new Web3(provider)
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts()
+    // account 0 is 0xbCa6F602D17d55374Bef6661D0298C7e41178bA2
+    const [account] = await web3.eth.getAccounts()
 
-    console.log('Attempting to deploy from account', accounts[0])
+    console.log('Attempting to deploy from account', account)
 
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
-        .send({ gas: '1000000', from: accounts[0] })
+        .send({ gas: '1000000', from: account })
 
+    // initial testnet contract address: 0x8723D47Bb28E3F5C7493f4Ee0c36bb9953c16B25
     console.log('Factory contract deployed to', result.options.address)
     provider.engine.stop()
 }
-deploy()
+void deploy()
