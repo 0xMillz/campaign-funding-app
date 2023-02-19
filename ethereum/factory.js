48f9a0fc713cb0288941365a114cb53659e6de10
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-18 18:06:37 -0700

    TURBO lint

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from './web3'
+import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  process.env.SEPOLIA_FACTORY_ADDRESS
-);
+    JSON.parse(CampaignFactory.interface),
+    process.env.SEPOLIA_FACTORY_ADDRESS
+)
 
-export default instance;
+export default instance
