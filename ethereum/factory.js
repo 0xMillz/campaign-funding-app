9f6a4578df3841247e029ccd2653889349b1b5b0
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-02-20 21:38:44 -0700

    Update code for newer version of web3

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,6 @@
-require('dotenv').config()
 import web3 from './web3'
 import CampaignFactory from './build/CampaignFactory.json'
 
-const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    process.env.SEPOLIA_FACTORY_ADDRESS
-)
+const instance = new web3.eth.Contract(CampaignFactory.abi, process.env.SEPOLIA_FACTORY_ADDRESS)
 
 export default instance
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,17 @@
 import Web3 from 'web3'
 
-window.ethereum.request({ method: 'eth_requestAccounts' })
+let web3
 
-const web3 = new Web3(window.ethereum)
+// browser && metamask
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+    web3 = new Web3(window.ethereum)
+} else {
+    // Server side or browser w/no metamask
+    const provider = new Web3.providers.HttpProvider(
+        process.env.SEPOLIA_TESTNET_ENDPOINT + process.env.INFURA_API_KEY
+    )
+    web3 = new Web3(provider)
+}
 
 export default web3
